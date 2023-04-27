fb9aab3ecb1c56105f4b9720ab33c0ef64378cde
GrandEmetak <JekHamond@tutanota.com>
2023-04-27 12:27:15 +0500

    Initial commit

---
+++ src/test/java/com/domain/reactor/StudentReactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domain.reactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentReactorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
