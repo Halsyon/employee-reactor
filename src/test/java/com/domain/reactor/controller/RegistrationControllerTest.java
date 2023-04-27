15e8fe6f382c8638ed446a20e0cbf37908ab683c
GrandEmetak <JekHamond@tutanota.com>
2023-04-27 15:07:10 +0500

    feat: created test layer

---
+++ src/test/java/com/domain/reactor/controller/RegistrationControllerTest.java
@@ -0,0 +1,161 @@
+/*
+Copyright [2023] [Halsyon]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+package com.domain.reactor.controller;
+
+import com.domain.reactor.model.dto.EmployeeDto;
+import com.domain.reactor.repository.EmployeeRepository;
+import com.domain.reactor.service.EmployeeServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+import java.util.Collections;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class RegistrationControllerTest {
+
+    @Autowired
+    private EmployeeServiceImpl employeeService;
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @BeforeEach
+    public void before(){
+        System.out.println("Before Each Test");
+        employeeRepository.deleteAll().subscribe();
+    }
+
+    @Test
+    public void testSaveEmployee(){
+
+        EmployeeDto employeeDto = new EmployeeDto();
+        employeeDto.setFirstName("Jona");
+        employeeDto.setLastName("Hill");
+        employeeDto.setEmail("jona@gmail.com");
+
+        webTestClient.post().uri("/api/employee")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(Mono.just(employeeDto), EmployeeDto.class)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody()
+                .consumeWith(System.out::println)
+                .jsonPath("$.firstName").isEqualTo(employeeDto.getFirstName())
+                .jsonPath("$.lastName").isEqualTo(employeeDto.getLastName())
+                .jsonPath("$.email").isEqualTo(employeeDto.getEmail());
+    }
+
+    @Test
+    public void testGetSingleEmployee(){
+
+        EmployeeDto employeeDto = new EmployeeDto();
+        employeeDto.setFirstName("Jona");
+        employeeDto.setLastName("Hill");
+        employeeDto.setEmail("jona@gmail.com");
+
+        EmployeeDto savedEmployee = employeeService.saveEmployee(employeeDto).block();
+
+        webTestClient.get().uri("/api/employee/{employeeId}", Collections.singletonMap("employeeId",savedEmployee.getId()))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .consumeWith(System.out::println)
+                .jsonPath("$.id").isEqualTo(savedEmployee.getId())
+                .jsonPath("$.firstName").isEqualTo(employeeDto.getFirstName())
+                .jsonPath("$.lastName").isEqualTo(employeeDto.getLastName())
+                .jsonPath("$.email").isEqualTo(employeeDto.getEmail());
+    }
+
+    @Test
+    public void testGetAllEmployees(){
+
+        EmployeeDto employeeDto = new EmployeeDto();
+        employeeDto.setFirstName("Jona");
+        employeeDto.setLastName("Hill");
+        employeeDto.setEmail("jona@gmail.com");
+
+        employeeService.saveEmployee(employeeDto).block();
+
+        EmployeeDto employeeDto1 = new EmployeeDto();
+        employeeDto1.setFirstName("Meena");
+        employeeDto1.setLastName("Fadatare");
+        employeeDto1.setEmail("meena@gmail.com");
+
+        employeeService.saveEmployee(employeeDto1).block();
+
+        webTestClient.get().uri("/api/employee")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(EmployeeDto.class)
+                .consumeWith(System.out::println);
+    }
+
+    @Test
+    public void testUpdateEmployee(){
+
+        EmployeeDto employeeDto = new EmployeeDto();
+        employeeDto.setFirstName("Jona");
+        employeeDto.setLastName("Hill");
+        employeeDto.setEmail("jona@gmail.com");
+
+        EmployeeDto savedEmployee = employeeService.saveEmployee(employeeDto).block();
+
+        EmployeeDto updatedEmployee = new EmployeeDto();
+        updatedEmployee.setFirstName("Sam");
+        updatedEmployee.setLastName("Bronson");
+        updatedEmployee.setEmail("Bronson@gmail.com");
+
+        webTestClient.put().uri("/api/employees/{employeeId}", Collections.singletonMap("employeeId", savedEmployee.getId()))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(Mono.just(updatedEmployee), EmployeeDto.class)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .consumeWith(System.out::println)
+                .jsonPath("$.firstName").isEqualTo(updatedEmployee.getFirstName())
+                .jsonPath("$.lastName").isEqualTo(updatedEmployee.getLastName())
+                .jsonPath("$.email").isEqualTo(updatedEmployee.getEmail());
+    }
+
+    @Test
+    public void testDeleteEmployee(){
+
+        EmployeeDto employeeDto = new EmployeeDto();
+        employeeDto.setFirstName("Jona");
+        employeeDto.setLastName("Hill");
+        employeeDto.setEmail("jona@gmail.com");
+
+        EmployeeDto savedEmployee = employeeService.saveEmployee(employeeDto).block();
+
+        webTestClient.delete().uri("/api/employee/{employeeId}", Collections.singletonMap("employeeId", savedEmployee.getId()))
+                .exchange()
+                .expectStatus().isNoContent()
+                .expectBody()
+                .consumeWith(System.out::println);
+
+    }
+}
