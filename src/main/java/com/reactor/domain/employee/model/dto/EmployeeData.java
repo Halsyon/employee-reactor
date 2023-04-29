5507fda49184d69af28285b5162ef7a26a7ac7fe
GrandEmetak <JekHamond@tutanota.com>
2023-04-29 20:35:16 +0500

    feat: creating new DTO EmployeeData

---
+++ src/main/java/com/reactor/domain/employee/model/dto/EmployeeData.java
@@ -0,0 +1,20 @@
+package com.reactor.domain.employee.model.dto;
+
+import com.reactor.domain.employee.model.entity.Employee;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeData {
+
+    private String employeeDataId;
+    private String description;
+    private LocalDateTime dateOfCompletion;
+
+    private EmployeeDto employee;
+}
